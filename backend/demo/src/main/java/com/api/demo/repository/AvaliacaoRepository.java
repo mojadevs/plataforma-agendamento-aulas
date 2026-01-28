e5b24ba6c98e1b60734a8292f326a7f4da309a0b
Mateus Abreu <abreufagundesmateus@gmail.com>
2026-01-28 20:07:35 -0300

    criação do diretório repository

---
+++ backend/demo/src/main/java/com/api/demo/repository/AvaliacaoRepository.java
@@ -0,0 +1,8 @@
+package com.api.demo.repository;
+
+import com.api.demo.model.Avaliacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AvaliacaoRepository extends JpaRepository<Avaliacao, Long> { }

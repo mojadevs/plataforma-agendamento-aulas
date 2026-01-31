02e0b6ca0e3556a19386fc53e77abb0dbad49c8b
Mateus Abreu <abreufagundesmateus@gmail.com>
2026-01-31 19:50:58 -0300

    Armazemanemto das hashs da senha no banco de dados

--- backend/demo/src/main/java/com/api/demo/dto/aluno/AlunoCreateDTO.java
+++ backend/demo/src/main/java/com/api/demo/dto/aluno/AlunoCreateDTO.java
@@ -5,8 +5,56 @@ import java.time.LocalDate;
 public class AlunoCreateDTO {
     private String nome;
     private String telefone;
+    private String senha;
     private Boolean ativo;
     private String email;
     private LocalDate data_cadastro;
 
+    public Boolean getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    public LocalDate getData_cadastro() {
+        return data_cadastro;
+    }
+
+    public void setData_cadastro(LocalDate data_cadastro) {
+        this.data_cadastro = data_cadastro;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 }

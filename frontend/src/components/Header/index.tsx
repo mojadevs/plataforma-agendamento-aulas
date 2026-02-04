5f071eccd3ff8db5852cc0250587343a5d5ca427
Jota <otavico.lima@email.com>
2026-02-03 23:40:26 -0300

    tela de cadastro de aluno

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,29 +1,22 @@
 import Link from "next/link";
+import styles from "./index.module.css";
 import CadastroBox from "../cadastroBox";
 
 export default function Header() {
   return (
-    <header className="w-full bg-(--cor-primaria) shadow-md">
-      <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
+    <header className={styles.header}>
+      <div className={styles.container}>
         
-        {/* LOGO */}
-        <Link
-          href="/"
-          className="text-2xl font-bold text-white hover:text-gray-300 transition"
-        >
+        <Link href="/" className={styles.logo}>
           JadeDrive
         </Link>
 
-        {/* AÇÕES */}
-        <nav className="flex gap-4">
-          <Link
-            href="/login"
-            className="px-5 py-2 border border-white text-white rounded-full hover:bg-(--cor-secundaria) transition"
-          >
+        <nav className={styles.actions}>
+          <Link href="/login" className={styles.login}>
             Login
           </Link>
 
-          <CadastroBox/>
+          <CadastroBox />
         </nav>
       </div>
     </header>
